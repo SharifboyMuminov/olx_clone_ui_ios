d76e317d06640c0b2fad1f7d0a57154e468c03c8
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-07-24 16:35:11 +0500

    Initial Commit

---
+++ olx_clone_ios/olx_clone_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  olx_clone_iosApp.swift
+//  olx_clone_ios
+//
+//  Created by Muminov Sharifjon on 24/07/25.
+//
+
+import SwiftUI
+
+@main
+struct olx_clone_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ olx_clone_iosTests/olx_clone_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  olx_clone_iosTests.swift
+//  olx_clone_iosTests
+//
+//  Created by Muminov Sharifjon on 24/07/25.
+//
+
+import Testing
+@testable import olx_clone_ios
+
+struct olx_clone_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
