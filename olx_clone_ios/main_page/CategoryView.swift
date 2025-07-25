3c6a82113617aebfffc4a36c7bc86658ecbb4747
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-07-25 11:05:02 +0500

    connect push next page

---
+++ olx_clone_ios/main_page/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  olx_clone_ios
+//
+//  Created by Muminov Sharifjon on 25/07/25.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
---
+++ olx_clone_ios/main_page/widget/CategoryItam.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryItam.swift
+//  olx_clone_ios
+//
+//  Created by Muminov Sharifjon on 25/07/25.
+//
+
+import SwiftUI
+
+struct CategoryItam: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryItam()
+}
