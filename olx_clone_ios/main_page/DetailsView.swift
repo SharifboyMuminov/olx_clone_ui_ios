b8cfba2269826b2ee9d8926c11bcd13734990208
muminovsharifjon <mumunovsharifboy@gmail.com>
2025-07-26 16:05:44 +0500

    end details page

---
+++ olx_clone_ios/main_page/DetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsView.swift
+//  olx_clone_ios
+//
+//  Created by Muminov Sharifjon on 25/07/25.
+//
+
+import SwiftUI
+
+struct DetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsView()
+}
---
+++ olx_clone_ios/main_page/widget/DetailsImageView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsImageView.swift
+//  olx_clone_ios
+//
+//  Created by Muminov Sharifjon on 26/07/25.
+//
+
+import SwiftUI
+
+struct DetailsImageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsImageView()
+}
